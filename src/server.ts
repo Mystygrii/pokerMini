02d6374361c4a33e2275a18eb0a19520f74e8c8d
Mystygrii <ado31794@gmail.com>
2024-05-15 19:39:01 +0200

    Cleaned the main file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
 import express from "express";
 import { gameRouter } from "../src/game/routes";
-import { makeDeck, shuffleDeck } from "./game/game";
-import { shuffle } from "vitest/utils.js";
 
 const app = express();
-
-const cardFamilies: Array<string> = ["Coeur", "Pique"];
-const cardValues: Array<string> = ["A", "K", "Q", "J", "10", "9"];
-
-const deck = makeDeck(cardFamilies, cardValues);
-console.log(shuffleDeck(deck));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
