0f323276e91d15441c18d33729bb6e5d30ee6fdc
Mystygrii <ado31794@gmail.com>
2024-05-15 19:38:31 +0200

    Added all the cards methods and their tests

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,42 +1,52 @@
 //Joueur
 export type Player = {
   name: string;
+  hand: Array<Card>;
+  jetons: number;
 };
 
 //cartes
 export type Card = {
   color: string;
-  value: string;
+  rank: string;
   Family: string;
+  value: number;
 };
-
-//variables
-
+//Valeurs et familles des cartes
+const cardValues: Array<string> = ["A", "K", "Q", "J", "10", "9"];
+const cardFamilies: Array<string> = ["Coeur", "Pique"];
 //Fonctions
-export function makeDeck(families: Array<string>, values: Array<string>) {
+
+//Créer les cartes et les ajoutes à la liste
+export function makeDeck(families: Array<string>, ranks: Array<string>) {
   var deck: Array<Card> = [];
 
   for (const i in families) {
-    for (const j in values) {
+    var value = 6;
+    for (const j in ranks) {
       if (families[i] === "Pique") {
         var card: Card = {
           color: "Noir",
-          value: values[j],
+          rank: ranks[j],
           Family: families[i],
+          value: value,
         };
       } else {
         var card: Card = {
           color: "Rouge",
-          value: values[j],
+          rank: ranks[j],
           Family: families[i],
+          value: value,
         };
       }
+      value--;
       deck.push(card);
     }
   }
   return deck;
 }
 
+//mélange du deck
 export function shuffleDeck(deck: Array<Card>) {
   var temporary: Card;
   for (const card in deck) {
@@ -49,6 +59,44 @@ export function shuffleDeck(deck: Array<Card>) {
   return deck;
 }
 
-export function dealCards(player: Player, deck: Array<Card>) {}
+//distribue une carte à chaque joueur
+export function dealCards(player: Player, deck: Array<Card>) {
+  if (deck.length > 0) {
+    var card = deck[-1];
+    player.hand.push(card);
+    deck.splice(-1, 1);
+  }
+}
 
-export function checkWin(player: Player) {}
+//Vérifie la combinaison dans la main du joueur
+export function checkHand(hand: Array<Card>) {
+  hand.sort((a, b) => a.value - b.value);
+  console.log(hand);
+  var playerHand = "";
+
+  //On regarde la main du joueur et on vérifie quelle est sa combinaison de cartes
+  for (var i = 0; i < hand.length - 1; i++) {
+
+    //Les 3 cartes se suivent et sont de la même famille
+    if ((hand[i + 1].value === hand[i].value + 1 && hand[0].value === hand[i].value-1) && (hand[i].Family === hand[i+1].Family && hand[i].Family === hand[0].Family)) {
+      playerHand = "Suite Flush";
+
+      //Les 3 cartes se suivent
+    }else if(hand[i + 1].value === hand[i].value + 1 && hand[0].value === hand[i].value-1){
+      playerHand = "Suite";
+
+      //Les 3 cartes sont de la même famille
+    }else if(hand[i].Family === hand[i+1].Family && hand[i].Family === hand[0].Family){
+      playerHand = "Flush";
+
+      //2 cartes ont la même valeur
+    }else if(hand[i + 1].rank === hand[i].rank || hand[0].rank === hand[i].rank ){
+      playerHand = "Pair";
+    
+      //Aucun des cas précédents
+    }else{
+      playerHand = "Carte Haute";
+    }
+  }
+  return playerHand;
+}
--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,23 +1,44 @@
 import { describe, expect, it } from "vitest";
-import { makeDeck, shuffleDeck } from "../src/game/game";
+import {
+  Player,
+  checkHand,
+  dealCards,
+  makeDeck,
+  shuffleDeck,
+} from "../src/game/game";
 
 const cardFamilies: Array<string> = ["Coeur", "Pique"];
 const cardValues: Array<string> = ["A", "K", "Q", "J", "10", "9"];
+const players: Array<Player> = [];
+const human: Player = {
+  name: "Mystygrii",
+  hand: [],
+  jetons: 100,
+};
+
+const bot: Player = {
+  name: "bot",
+  hand: [],
+  jetons: 100,
+};
+
+players.push(human);
+players.push(bot);
 
 //Deck par défaut, sans mélange appliqué
 const exampleDeck = [
-  { color: "Rouge", value: "A", Family: "Coeur" },
-  { color: "Rouge", value: "K", Family: "Coeur" },
-  { color: "Rouge", value: "Q", Family: "Coeur" },
-  { color: "Rouge", value: "J", Family: "Coeur" },
-  { color: "Rouge", value: "10", Family: "Coeur" },
-  { color: "Rouge", value: "9", Family: "Coeur" },
-  { color: "Noir", value: "A", Family: "Pique" },
-  { color: "Noir", value: "K", Family: "Pique" },
-  { color: "Noir", value: "Q", Family: "Pique" },
-  { color: "Noir", value: "J", Family: "Pique" },
-  { color: "Noir", value: "10", Family: "Pique" },
-  { color: "Noir", value: "9", Family: "Pique" },
+  { color: 'Rouge', rank: 'A', Family: 'Coeur', value: 6 }, 
+  { color: 'Rouge', rank: 'K', Family: 'Coeur', value: 5 }, 
+  { color: 'Rouge', rank: 'Q', Family: 'Coeur', value: 4 }, 
+  { color: 'Rouge', rank: 'J', Family: 'Coeur', value: 3 }, 
+  { color: 'Rouge', rank: '10', Family: 'Coeur', value: 2 },
+  { color: 'Rouge', rank: '9', Family: 'Coeur', value: 1 }, 
+  { color: 'Noir', rank: 'A', Family: 'Pique', value: 6 },  
+  { color: 'Noir', rank: 'K', Family: 'Pique', value: 5 },  
+  { color: 'Noir', rank: 'Q', Family: 'Pique', value: 4 },  
+  { color: 'Noir', rank: 'J', Family: 'Pique', value: 3 },  
+  { color: 'Noir', rank: '10', Family: 'Pique', value: 2 }, 
+  { color: 'Noir', rank: '9', Family: 'Pique', value: 1 }
 ];
 
 describe("A test to check all the pokerMini functions", () => {
@@ -34,4 +55,60 @@ describe("A test to check all the pokerMini functions", () => {
       expect(shuffledDeck[card] !== deck[card]).toBeFalsy();
     }
   });
+
+  it("must deal a card to each players", () => {
+    const deck = shuffleDeck(exampleDeck);
+    for (const player in players) {
+      dealCards(players[player], deck);
+      expect(players[player].hand.length).toBeGreaterThan(0);
+    }
+  });
+
 });
+
+describe('Testing the handCheck funtion', ()=>{
+  it("must be Suite Flush", () => {
+    (human.hand = [
+      { color: "Rouge", rank: "A", Family: "Coeur",value:6 },
+      { color: "Rouge", rank: "K", Family: "Coeur",value:5 },
+      { color: 'Rouge', rank: 'Q', Family: 'Coeur', value: 4 },
+    ]),
+      expect(checkHand(human.hand)).toBe("Suite Flush");
+  });
+
+  it("must be Suite", () => {
+    (human.hand = [
+      { color: "Rouge", rank: "K", Family: "Coeur",value:5 },
+      { color: "Rouge", rank: "A", Family: "Coeur",value:6 },
+      { color: 'Rouge', rank: 'Q', Family: 'Pique', value: 4 },
+    ]),
+      expect(checkHand(human.hand)).toBe("Suite");
+  });
+
+  it("must be Flush", () => {
+    (human.hand = [
+      { color: "Noir", rank: "10", Family: "Coeur",value:2 },
+      { color: "Rouge", rank: "K", Family: "Coeur",value:5 },
+      { color: 'Rouge', rank: 'J', Family: 'Coeur', value: 3 },
+    ]),
+      expect(checkHand(human.hand)).toBe("Flush");
+  });
+
+  it("must be Pair", () => {
+    (human.hand = [
+      { color: "Rouge", rank: "A", Family: "Coeur",value:6 },
+      { color: "Noir", rank: "A", Family: "Pique",value:6 },
+      { color: 'Noir', rank: '9', Family: 'Pique', value: 1 },
+    ]),
+      expect(checkHand(human.hand)).toBe("Pair");
+  });
+
+  it("must be Carte Haute", () => {
+    (human.hand = [
+      { color: "Rouge", rank: "A", Family: "Coeur",value:6 },
+      { color: "Noir", rank: "10", Family: "Pique",value:2 },
+      { color: 'Noir', rank: 'K', Family: 'Pique', value: 5 },
+    ]),
+      expect(checkHand(human.hand)).toBe("Carte Haute");
+  });
+})
